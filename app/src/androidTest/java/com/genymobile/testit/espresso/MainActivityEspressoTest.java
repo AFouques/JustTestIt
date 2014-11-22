3f237877878124f68d70ae40127b4c13d630bd98
Sylvain Galand <sgaland@genymobile.com>
2014-11-22 12:14:59 +0100

    Add Genymotion JAVA Api test

--- app/src/androidTest/java/com/genymobile/testit/espresso/MainActivityEspressoTest.java
+++ app/src/androidTest/java/com/genymobile/testit/espresso/MainActivityEspressoTest.java
@@ -1,10 +1,13 @@
 package com.genymobile.testit.espresso;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.test.ActivityInstrumentationTestCase2;
 import android.view.View;
 
 import com.genymobile.testit.MainActivity;
 import com.genymobile.testit.R;
+import com.genymotion.api.GenymotionManager;
 
 import static android.test.ViewAsserts.assertOnScreen;
 
@@ -26,5 +29,32 @@ public class MainActivityEspressoTest extends ActivityInstrumentationTestCase2<M
 
     public void testBatteryLevelViewIsShown() {
         assertOnScreen(mainActivity.getWindow().getDecorView(), batteryLevelView);
+    }
+
+    public void testWhenBatteryLevelIsMoreThanHalfLevelViewIsGreen() throws Exception {
+
+        // Avoid the test if not a Genymotion device
+        if (!GenymotionManager.isGenymotionDevice()) {
+            return;
+        }
+        GenymotionManager.getGenymotionManager(getActivity()).getBattery().setLevel(100);
+
+        View levelView = getActivity().findViewById(R.id.battery_level_view);
+        ColorDrawable green = (ColorDrawable) levelView.getBackground();
+        assertEquals(green.getColor(), Color.GREEN);
+    }
+
+    public void testWhenBatteryLevelIsLessThanHalfLevelViewIsRed() throws Exception {
+
+        // Avoid the test if not a Genymotion device
+        if (!GenymotionManager.isGenymotionDevice()) {
+            return;
+        }
+
+        GenymotionManager.getGenymotionManager(getActivity()).getBattery().setLevel(10);
+
+        View levelView = getActivity().findViewById(R.id.battery_level_view);
+        ColorDrawable green = (ColorDrawable) levelView.getBackground();
+        assertEquals(green.getColor(), Color.RED);
     }
 }
