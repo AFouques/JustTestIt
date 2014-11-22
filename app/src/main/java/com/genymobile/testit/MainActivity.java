3546b62957bd04c257a43ad44fc2e8f424fb9bff
Sylvain Galand <sgaland@genymobile.com>
2014-11-22 12:01:34 +0100

    Listen to battery changes

--- app/src/main/java/com/genymobile/testit/MainActivity.java
+++ app/src/main/java/com/genymobile/testit/MainActivity.java
@@ -1,6 +1,8 @@
 package com.genymobile.testit;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Color;
@@ -12,25 +14,26 @@ public class MainActivity extends Activity {
 
     private View batteryLevelView;
 
+    private BatteryLevelListener batteryLevelListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         batteryLevelView = findViewById(R.id.battery_level_view);
+
+        batteryLevelListener = new BatteryLevelListener();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
-        updateBatteryLevel(getCurrentBatteryWatcher());
-    }
-
-    private BatteryWatcher getCurrentBatteryWatcher() {
         IntentFilter intentFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
-        Intent batteryStatus = this.registerReceiver(null, intentFilter);
-        return new BatteryWatcher(batteryStatus);
+        Intent batteryStatus = this.registerReceiver(batteryLevelListener, intentFilter);
+
+        updateBatteryLevel(new BatteryWatcher(batteryStatus));
     }
 
     private void updateBatteryLevel(BatteryWatcher watcher) {
@@ -38,6 +41,13 @@ public class MainActivity extends Activity {
             batteryLevelView.setBackgroundColor(Color.GREEN);
         } else {
             batteryLevelView.setBackgroundColor(Color.RED);
+        }
+    }
+
+    private class BatteryLevelListener extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            updateBatteryLevel(new BatteryWatcher(intent));
         }
     }
 }
